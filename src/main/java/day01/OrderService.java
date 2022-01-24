d97198ebed93d09cc4292140cf4afe87c1674367
ht0m <hartht@gmail.com>
2022-01-24 18:59:31 +0100

    Initial commit

---
+++ src/main/java/day01/OrderService.java
@@ -0,0 +1,49 @@
+package day01;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OrderService {
+
+    private List<Order> orders = new ArrayList<>();
+
+
+    public void saveOrder(Order order) {
+        orders.add(order);
+    }
+
+    public long getNumberOfOrders(String s) {
+
+        return orders.stream()
+                .filter(o -> o.getStatus().equals(s))
+                .count();
+    }
+
+    public List<Order> getOrdersBetweenDates(LocalDate date1, LocalDate date2) {
+
+        return orders.stream()
+                .filter(o -> o.getOrderDate().isAfter(date1) && o.getOrderDate().isBefore(date2))
+                .collect(Collectors.toList());
+    }
+
+    public boolean IsLessProductThan(int min) {
+        return orders.stream()
+                .anyMatch(o -> o.getProducts().size() < min);
+    }
+
+    public Order getOrderWithMaxNumberOfProducts() {
+        return orders.stream()
+                .max(Comparator.comparing(order -> order.getProducts().size()))
+                .orElseThrow(() -> new IllegalStateException("Empty"));
+    }
+
+    public List<Order> getOrdersWithCategory(String s) {
+        return orders.stream()
+                .filter(o -> o.getProducts().stream().anyMatch(p -> p.getCategory().equals(s))).toList();
+    }
+
+}
